743575f37bc710da8983ccebd34e85d436d5fdab
Ajay Nautiyal <aju@nautiyal.co.in>
2019-09-18 23:25:31 +0530

    Initial commit

---
+++ precache-manifest.79c73b8b5b247702bb510b3d070dff08.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bddeeafb73b5c8c96005e23b8ee9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a65caea9c7f919531b44",
+    "url": "/static/css/2.24bf1742.chunk.css"
+  },
+  {
+    "revision": "207021b1c2923076d13f",
+    "url": "/static/css/main.80abdffa.chunk.css"
+  },
+  {
+    "revision": "a65caea9c7f919531b44",
+    "url": "/static/js/2.ada8a20b.chunk.js"
+  },
+  {
+    "revision": "207021b1c2923076d13f",
+    "url": "/static/js/main.82a09d9a.chunk.js"
+  },
+  {
+    "revision": "a5a45d48ddc82be02a63",
+    "url": "/static/js/runtime~main.9686fe7e.js"
+  },
+  {
+    "revision": "fed40d23b964f3221072a8e6228146a6",
+    "url": "/static/media/ajay.fed40d23.svg"
+  },
+  {
+    "revision": "9d095907f571353ba85eb8fd91387c61",
+    "url": "/static/media/book.9d095907.svg"
+  },
+  {
+    "revision": "d1237c57aca97b51eeb9a6e44f3f3353",
+    "url": "/static/media/clipboard.d1237c57.svg"
+  },
+  {
+    "revision": "5a55a3cfb77b2976b045cf9a6a1ba36f",
+    "url": "/static/media/comment-alt.5a55a3cf.svg"
+  },
+  {
+    "revision": "8784386388b9b48e4e5e739a0a8e9492",
+    "url": "/static/media/desktop.87843863.svg"
+  },
+  {
+    "revision": "0ac6bb5939f5b57f9e8845bf365ea038",
+    "url": "/static/media/email.0ac6bb59.svg"
+  },
+  {
+    "revision": "aa75a85c5ab03032e1414c990ea86573",
+    "url": "/static/media/github.aa75a85c.svg"
+  },
+  {
+    "revision": "f3216da855c8b5b7d469b03ec2f0cba4",
+    "url": "/static/media/home-bg.f3216da8.jpg"
+  },
+  {
+    "revision": "ef1122b1c2a3efe1dfa32c8b6de6601a",
+    "url": "/static/media/home.ef1122b1.svg"
+  },
+  {
+    "revision": "cb9c2a6f8db04d09d33a06a30f69eade",
+    "url": "/static/media/linkedin.cb9c2a6f.svg"
+  },
+  {
+    "revision": "9af72a628257a66bc02db4ff42b5715b",
+    "url": "/static/media/location-pin.9af72a62.svg"
+  },
+  {
+    "revision": "1344d17d3d128e5007b78d7e98b18b0f",
+    "url": "/static/media/menu.1344d17d.svg"
+  },
+  {
+    "revision": "975d72447ae1768a637e3dc8e14c71e8",
+    "url": "/static/media/mobile.975d7244.svg"
+  },
+  {
+    "revision": "717ae01394f9d3561c58382e135c4bd0",
+    "url": "/static/media/twitter-alt.717ae013.svg"
+  },
+  {
+    "revision": "ac642e61ec60797c116792eb1910ca8a",
+    "url": "/static/media/user.ac642e61.svg"
+  }
+]);
